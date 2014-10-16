e20b5160633d5c424dc5226de9ef46e0c77bf376
anand <anand21nanda@gmail.com>
2014-10-16 20:30:54 +0530

    added a release file

---
+++ fadapa/release.py
@@ -0,0 +1,11 @@
+NAME = 'fadapa'
+VERSION = '0.1'
+DESCRIPTION = 'Minimal & efficient parser for FastQC output data in Python'
+LONG_DESCRIPTION = '''Fadapa is a minimal and efficient parser for parsing Fastqc data. Using this module parsing FastQC results is quite easy and simple.'''
+AUTHOR = 'Anand Reddy Pandikunta'
+EMAIL = 'anand21nanda@gmail.com'
+COPYRIGHT = 'Uncopyrighted'
+URL = 'https://github.com/fadapa/fadapa'
+LICENSE = 'Uncopyrighted'
+
+__all__ = ('NAME', 'VERSION', 'DESCRIPTION', 'LONG_DESCRIPTION', 'AUTHOR', 'EMAIL', 'COPYRIGHT', 'URL', 'LICENSE')
