e010836ebd309edbcfbb983dcf66a09a0ad794cd
anand <anand21nanda@gmail.com>
2014-11-04 20:10:40 +0530

    fixed bugs, changed md to rst

---
+++ README.rst
@@ -0,0 +1,68 @@
+Welcome to Fadapa
+-----------------
+
+FAstqc DAta PArser - The Easiest way to parse FastQC results.
+
+|Build| |Coverage| |Latest Version| |Supported Python versions|
+
+Installation
+------------
+1. Recomended way to install is using ``pip``
+::
+
+    pip install fadapa
+
+2. Alternatively you can install with ``esay_install``
+::
+
+   easy_install fadapa
+
+3. You can also install from Github source code.
+::
+
+   cd 
+   git clone https://github.com/fadapa/fadapa.git 
+   cd fadapa
+   python setup.py install
+
+Usage
+-----
+
+::
+
+    # import fadapa
+    from fadapa import Fadapa
+
+    #load file
+    f = Fadapa('/path/to/fastqc_output_file.txt')
+
+    #get file name
+    print(f.file_name)
+
+    #get entire content
+    print(f.content())
+
+    #get all module names and their status
+    print(f.summary())
+
+    #get raw data of any module
+    print(f.raw_module('module name'))
+
+    #get cleaned data of any module
+    print(f.clean_data('module name'))
+
+Author
+------
+
+`Anand Reddy Pandikunta`_
+
+.. _Anand Reddy Pandikunta: http://www.avilpage.com
+
+.. |Build| image:: https://api.travis-ci.org/fadapa/fadapa.png?branch=master
+   :target: http://travis-ci.org/fadapa/fadapa/
+.. |Coverage| image:: https://coveralls.io/repos/fadapa/fadapa/badge.png?branch=master
+   :target: https://coveralls.io/r/fadapa/fadapa?branch=master
+.. |Latest Version| image:: https://pypip.in/version/fadapa/badge.svg?text=version
+   :target: https://pypi.python.org/pypi/fadapa/
+.. |Supported Python versions| image:: https://pypip.in/py_versions/fadapa/badge.svg
+   :target: https://pypi.python.org/pypi/fadapa/
--- fadapa/fadapa.py
+++ fadapa/fadapa.py
@@ -24,17 +24,13 @@ class Fadapa(object):
         Returns a list of all modules present in the content.
         Module begins  with  '>>Module Name',  ends with '>>END_MODULE'
         """
-        try:
-            data = []
-            for line in self._content:
-                if self._m_mark in line and not self._m_end in line:
-                    module_name, status = line.split('\t')
-                    data.append([status, module_name[2:]])
-            data.insert(0, ['Status', 'Module Name'])
-            return data
-        except:          # pragma: no cover
-            raise "It is not a valid FastQC file. \
-                   Please specify a correct file."
+        data = []
+        for line in self._content:
+            if self._m_mark in line and not self._m_end in line:
+                module_name, status = line.split('\t')
+                data.append([status, module_name[2:]])
+        data.insert(0, ['Status', 'Module Name'])
+        return data
 
     def content(self):
         """
@@ -50,15 +46,11 @@ class Fadapa(object):
         :arg module: name of module as returned by _moduels function.
         :type module: str
         """
-        try:
-            con = self._content
-            s_pos = next(con.index(x) for x in con if module in x)
-            e_pos = con[s_pos:].index(self._m_end)
-            raw_data = con[s_pos:s_pos+e_pos+1]
-            return raw_data
-        except:               # pragma: no cover
-            raise "Please specify correct module name as mentioned \
-                    in summary method."
+        con = self._content
+        s_pos = next(con.index(x) for x in con if module in x)
+        e_pos = con[s_pos:].index(self._m_end)
+        raw_data = con[s_pos:s_pos+e_pos+1]
+        return raw_data
 
     def clean_data(self, module):
         """
@@ -67,12 +59,9 @@ class Fadapa(object):
         :arg module: name of module
         :type module: str
         """
-        try:
-            filtered_data = self.raw_data(module)[1:-1]
-            data_list = [filter(None, x.split('\t'))  \
-                         for x in filtered_data]
-            data_list[0][0] = data_list[0][0][1:] 
-            return data_list
-        except:                # pragma: no cover
-            raise "Please specify correct module name as mentioned \
-                   in summary method."
+        filtered_data = self.raw_data(module)[1:-1]
+        data_list = [list(filter(None, x.split('\t')))  \
+                     for x in filtered_data]
+        print(data_list)
+        data_list[0][0] = data_list[0][0][1:] 
+        return data_list
