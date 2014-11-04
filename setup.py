5a2617551a66407bc8a1ddf29ffa94b7bd826d24
anand <anand21nanda@gmail.com>
2014-11-04 23:01:23 +0530

    removed 2.6 from supported versions

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ setup(name='fadapa',
           'Operating System :: POSIX',
           
           'Programming Language :: Python', 
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6', 
           'Programming Language :: Python :: 2.7', 
           'Programming Language :: Python :: 3', 
           'Programming Language :: Python :: 3.3',
